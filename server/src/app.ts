d9adbe28728f95b993e4488ee95ad405a24801f4
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-22 17:24:48 +0200

    prettier formatted

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
-import cookieSession from "cookie-session";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import * as Yup from "yup";
-import userRouter from "./resources/users/user-router";
+import cookieSession from 'cookie-session';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import * as Yup from 'yup';
+import userRouter from './resources/users/user-router';
 
 export const app = express();
 
@@ -12,7 +12,7 @@ app.use(
   cookieSession({
     secure: false,
     httpOnly: true,
-    secret: "fjkarhgoahgbvjbjaerhfaorsafa",
+    secret: 'fjkarhgoahgbvjbjaerhfaorsafa',
     maxAge: 3600000,
   }),
 );
@@ -28,7 +28,7 @@ app.use(userRouter);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   if (err instanceof Yup.ValidationError) {
-    if (err.message.includes("errors occurred")) {
+    if (err.message.includes('errors occurred')) {
       res.status(400).json(err.errors[0]);
     } else {
       res.status(400).json(err.message);
