de15f21ad4c3cf48f58902b9801b26fdf9669914
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-22 23:06:33 +0200

    added functionality for login and logout

--- client/src/components/UserDropdownMenu.tsx
+++ client/src/components/UserDropdownMenu.tsx
@@ -1,6 +1,7 @@
 import { Button, Menu, Text } from '@mantine/core';
 import {
   IconArrowsLeftRight,
+  IconLogin,
   IconLogout,
   IconSearch,
   IconSettings,
@@ -8,8 +9,28 @@ import {
   IconUser,
   IconUserCircle,
 } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserContext } from '../contexts/UseUserContext';
 
 function UserDropdownMenu() {
+  const { logout, user } = useUserContext();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoggedIn(!!user);
+  }, [user]);
+
+  const handleLogout = async () => {
+    try {
+      await logout(); // Call the logout function
+      console.log('User has been signed out :)');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
@@ -19,10 +40,30 @@ function UserDropdownMenu() {
       </Menu.Target>
 
       <Menu.Dropdown>
-        <Menu.Label>Application</Menu.Label>
-        <Menu.Item icon={<IconUser size={14} />}>My account</Menu.Item>
-        <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-        <Menu.Item icon={<IconLogout size={14} />}>Sign out</Menu.Item>
+        {isLoggedIn ? (
+          <>
+            <Menu.Item icon={<IconUser size={14} />}>My account</Menu.Item>
+            <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
+            <Menu.Item icon={<IconLogout size={14} />} onClick={handleLogout}>
+              Sign out
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Menu.Item
+              icon={<IconLogin size={14} />}
+              onClick={() => navigate('/signin')}
+            >
+              Sign in
+            </Menu.Item>
+            <Menu.Item
+              icon={<IconUser size={14} />}
+              onClick={() => navigate('/createaccount')}
+            >
+              Create an account
+            </Menu.Item>
+          </>
+        )}
         <Menu.Item
           icon={<IconSearch size={14} />}
           rightSection={
