b30d827c03f715b6b74ec9f0c4434bda6008859d
Moa Hedendahl <moahedendahl@gmail.com>
2023-05-22 10:10:02 +0200

    added router for logout

--- server/src/resources/users/user-router.ts
+++ server/src/resources/users/user-router.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { loginUser, registerUser } from "./user-controller";
+import { loginUser, logoutUser, registerUser } from "./user-controller";
 
 const userRouter = express
   .Router()
   .post("/api/users/register", registerUser)
-  .post("/api/users/login", loginUser);
+  .post("/api/users/login", loginUser)
+  .post("/api/users/logout", logoutUser);
 export default userRouter;
