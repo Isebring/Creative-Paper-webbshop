cb53f1e83c6fb827edf5702dace15162ec1283f8
Moa Hedendahl <moahedendahl@gmail.com>
2023-05-22 09:39:33 +0200

    Added a schema for the user

---
+++ server/src/resources/users/user-model.ts
@@ -0,0 +1,40 @@
+import argon2 from "argon2";
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      minlength: [3, "Username must be at least 3 characters"],
+      maxlength: [32, "Username cannot exceed 32 characters"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [5, "Password must be at least 5 characters"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: true, updatedAt: false },
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  // kryptera lösenordet
+  this.password = await argon2.hash(this.password, {
+    memoryCost: 1024,
+    timeCost: 2,
+  });
+  next();
+});
+
+export type User = InferSchemaType<typeof userSchema>;
+
+export const UserModel = model("user", userSchema);
