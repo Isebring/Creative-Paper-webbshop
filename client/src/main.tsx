f708c5b03001cc7b96e16135cb730a77aaefd9aa
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-22 13:34:05 +0200

    changed signup to createaccount

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,12 +10,12 @@ import Admin from './pages/Admin';
 import Cart from './pages/Cart';
 import Confirmation from './pages/Confirmation';
 import { Contact } from './pages/Contact';
+import CreateAccount from './pages/CreateAccount';
 import EditProduct from './pages/EditProduct';
 import { Faq } from './pages/Faq';
 import Home from './pages/Home';
 import NewProduct from './pages/NewProduct';
 import ProductDetails from './pages/ProductDetails';
-import SignUp from './pages/SignUp';
 import Root from './root';
 
 export const router = createBrowserRouter(
@@ -31,7 +31,7 @@ export const router = createBrowserRouter(
       <Route path="/admin/product/:id/edit" element={<EditProduct />} />
       <Route path="/admin/product/new" element={<NewProduct />} />
       <Route path="/confirmation" element={<Confirmation />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/createaccount" element={<CreateAccount />} />
     </Route>,
   ),
 );
