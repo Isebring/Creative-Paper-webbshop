b4ac023f756e232f78aa9af75a6b1c74332ea121
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-25 00:44:36 +0200

    added app.use(orderRouter)

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error-handler';
+import orderRouter from './resources/orders/order-router';
 import productRouter from './resources/products/product-router';
 import userRouter from './resources/users/user-router';
 
@@ -19,7 +20,7 @@ app.use(
 );
 
 // Routes
-// app.use(orderRouter);
+app.use(orderRouter);
 // app.use(categoryRouter);
 app.use(productRouter);
 // app.use(imageRouter);
