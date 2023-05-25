dcf645dac9b5eeb3f2e51a80ef6dbb96883a7333
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 01:15:52 +0200

    commented out function, don't use it right now

--- server/src/middlewares/auth-user.ts
+++ server/src/middlewares/auth-user.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+// import express from 'express';
 
-export function isUserAuthenticated(
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) {
-  if (req.session?.userId) {
-    next();
-  } else {
-    res.status(401).json('You must be logged in to perform this action');
-  }
-}
+// export function isUserAuthenticated(
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction,
+// ) {
+//   if (req.session?.userId) {
+//     next();
+//   } else {
+//     res.status(401).json('You must be logged in to perform this action');
+//   }
+// }
