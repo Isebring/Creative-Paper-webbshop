16c619a007f1ba55e35620c2770909e71df657a6
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-25 18:47:34 +0200

    added price and removed virtual property

--- server/src/resources/orders/order-model.ts
+++ server/src/resources/orders/order-model.ts
@@ -1,5 +1,4 @@
-import { InferSchemaType, Schema, Types, model } from 'mongoose';
-import { Product } from '../products/product-model';
+import { InferSchemaType, Schema, model } from 'mongoose';
 
 const orderItemSchema = new Schema(
   {
@@ -12,18 +11,15 @@ const orderItemSchema = new Schema(
       type: Number,
       required: true,
     },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
   {
     _id: false, // Ska MongoDB ge ett id till varje orderItem?!
-    toJSON: { virtuals: true },
   },
 );
-// Virtual property for price
-orderItemSchema.virtual('price').get(function () {
-  // Calculate the price based on the product or any other logic
-  if (Types.ObjectId.isValid(this.product)) return 0;
-  return this.quantity * (this.product as unknown as Product).price; // Replace with your logic
-});
 
 const orderSchema = new Schema(
   {
