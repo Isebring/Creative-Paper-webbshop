e2067c780d43a96e52dfa34f3bfe7ce04f7f7d2a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 01:16:08 +0200

    edited global error handler

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,16 +1,42 @@
 import { ErrorRequestHandler } from 'express';
 import { ValidationError } from 'yup';
 
+export class BadRequestError extends Error {
+  statusCode = 400;
+  constructor(message = 'Bad Request') {
+    super(message);
+    this.name = 'BadRequestError';
+  }
+}
+
+export class UnauthorizedError extends Error {
+  statusCode = 401;
+  constructor(message = 'Unauthorized') {
+    super(message);
+    this.name = 'UnauthorizedError';
+  }
+}
+
+export class NotFoundError extends Error {
+  statusCode = 404;
+  constructor(message = 'Not Found') {
+    super(message);
+    this.name = 'NotFoundError';
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err);
 
   if (err instanceof ValidationError) {
-    if (err.message.includes('errors occurred')) {
-      res.status(400).json(err.errors[0]);
-    } else {
-      res.status(400).json(err.message);
-    }
+    res.status(400).json(err.message);
+  } else if (
+    err instanceof BadRequestError ||
+    err instanceof UnauthorizedError ||
+    err instanceof NotFoundError
+  ) {
+    res.status(err.statusCode).json(err.message);
   } else if (err instanceof Error) {
     res.status(500).json(err.message);
   } else {
