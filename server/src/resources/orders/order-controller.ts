9118108898572b359c9be72a7a6be57a5196f53f
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 01:16:27 +0200

    implemented global error handler in functions

--- server/src/resources/orders/order-controller.ts
+++ server/src/resources/orders/order-controller.ts
@@ -1,105 +1,107 @@
 import { Request, Response } from 'express';
 import { Types as MongooseTypes } from 'mongoose';
-import * as Yup from 'yup';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../../middlewares/error-handler';
 import { ProductModel } from '../products/product-model';
 import { UserModel } from '../users/user-model';
 import { OrderModel } from './order-model';
 import orderValidationSchema from './order-validation';
 
+// Get orders
 export async function getOrders(req: Request, res: Response) {
-  // const products = await ProductModel.find();
-  // res.status(200).json(products);
+  // Verify if the user is logged in
+  if (!req.session?.userId) {
+    throw new UnauthorizedError('You must login to view your orders');
+  }
+  // Retrieve user's orders from the database
+  const orders = await OrderModel.find({ user: req.session.userId });
+  res.status(200).json({
+    success: true,
+    data: orders,
+  });
 }
+
+// Get all orders
 export async function getAllOrders(req: Request, res: Response) {
   const products = await OrderModel.find({}).populate('orderItems.product');
   res.status(200).json(products);
 }
 
+// Create order
 export async function createOrder(req: Request, res: Response) {
-  try {
-    // Verify if the user is logged in
-    if (!req.session?.userId) {
-      res.status(401).json('You must login to place an order');
-      return;
-    }
-
-    // Validate request body
-    const validatedBody = await orderValidationSchema.validate(req.body);
-
-    // Retrieve the user from the database
-    const user = await UserModel.findById(req.session.userId);
-    if (!user) {
-      res.status(404).json('User not found');
-      return;
-    }
-
-    // Process each orderItem to include price and calculate total price
-    const orderItems = [];
-    let totalPrice = 0;
-    for (const item of validatedBody.orderItems) {
-      const product = await ProductModel.findById(item.product);
-      if (!product) {
-        res.status(404).json('Product not found');
-        return;
-      }
-      const price = product.price;
-      const totalItemPrice = price * item.quantity;
-      orderItems.push({ ...item, price: totalItemPrice }); // price here is total price for this item
-      totalPrice += totalItemPrice;
-    }
-
-    // Create the order with the associated user
-    const orderData = {
-      user: user._id,
-      orderItems,
-      totalPrice,
-      deliveryAddress: validatedBody.deliveryAddress,
-      status: 'in progress',
-    };
-    const order = new OrderModel(orderData);
-    const savedOrder = await order.save();
-
-    res.status(201).json({
-      success: true,
-      data: savedOrder,
-    });
-  } catch (error) {
-    if (error instanceof Yup.ValidationError) {
-      // Validation failed
-      res.status(400).json({
-        success: false,
-        message: error.message,
-      });
-    } else if (error instanceof Error) {
-      // Other error
-      console.error(error.message);
-      res.status(500).json({
-        success: false,
-        message: error.message, // send the error message to the client
-      });
-    } else {
-      // handle other types of errors, or rethrow
-      throw error;
-    }
+  // Verify if the user is logged in
+  if (!req.session?.userId) {
+    throw new UnauthorizedError('You must login to place an order');
   }
+
+  // Validate request body
+  const validatedBody = await orderValidationSchema
+    .validate(req.body)
+    .catch((error) => {
+      throw new BadRequestError(error.message);
+    });
+
+  // Retrieve the user from the database
+  const user = await UserModel.findById(req.session.userId);
+  if (!user) {
+    throw new NotFoundError(`User with id ${req.session.userId} not found.`);
+  }
+
+  // Process each orderItem to include price and calculate total price
+  const orderItems = [];
+  let totalPrice = 0;
+  for (const item of validatedBody.orderItems) {
+    const product = await ProductModel.findById(item.product);
+    if (!product) {
+      throw new NotFoundError('Product not found');
+    }
+    const price = product.price;
+    const totalItemPrice = price * item.quantity;
+    orderItems.push({ ...item, price: totalItemPrice }); // price here is total price for this item
+    totalPrice += totalItemPrice;
+  }
+
+  // Create the order with the associated user
+  const orderData = {
+    user: user._id,
+    orderItems,
+    totalPrice,
+    deliveryAddress: validatedBody.deliveryAddress,
+    status: 'in progress',
+  };
+  const order = new OrderModel(orderData);
+  const savedOrder = await order.save();
+
+  res.status(201).json({
+    success: true,
+    data: savedOrder,
+  });
 }
 
+// Get order by id
 export async function getOrderById(req: Request, res: Response) {
   const orderId = req.params.id;
-  // Check if the provided postId is a valid ObjectId
+
+  // Check if the provided orderId is a valid ObjectId
   if (!MongooseTypes.ObjectId.isValid(orderId)) {
-    return res.status(400).json({ error: `Invalid order ID.` });
+    throw new BadRequestError('Invalid order ID.');
   }
+
   const order = await OrderModel.findById(orderId).populate(
     'orderItems.product',
   );
-  if (order) {
-    res.status(200).json(order);
-  } else {
-    res.status(404).json(`/${orderId} not found.`);
+
+  if (!order) {
+    throw new NotFoundError(`Order with id ${orderId} not found.`);
   }
+
+  res.status(200).json(order);
 }
 
+// Update order status
 export async function updateOrderStatus(req: Request, res: Response) {
   // const products = await ProductModel.find();
   // res.status(200).json(products);
