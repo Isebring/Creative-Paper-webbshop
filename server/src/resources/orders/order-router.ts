4b6ad4beecca29235f4a28cafeb646048a0e514f
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 23:25:10 +0200

    adjusted routes

--- server/src/resources/orders/order-router.ts
+++ server/src/resources/orders/order-router.ts
@@ -9,9 +9,9 @@ import {
 
 const orderRouter = express
   .Router()
-  .get('/api/orders', getOrders) // Get current user's orders
+  .get('/api/orders/user', getOrders) // Get current user's orders
   .get('/api/orders/all', isAdmin, getAllOrders) // Get all orders (admin only)
   .post('/api/orders', createOrder) // Create a new order
-  .get('/api/orders/:id', getOrderById); // Get a specific order by ID
+  .get('/api/orders/:_id', getOrderById); // Get a specific order by ID
 // .put('/api/orders/status/:id', updateOrderStatus); // Update the status of a specific order by ID
 export default orderRouter;
