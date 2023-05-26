60d62703536172334f845abd4fc1a42559f0256b
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 09:08:36 +0200

    seems like we need this route :)

--- server/src/resources/users/user-router.ts
+++ server/src/resources/users/user-router.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { isUserAuthenticated } from '../../middlewares/auth-user';
 import {
   getAllUsers,
+  getLoggedInUser,
   loginUser,
   logoutUser,
   registerUser,
@@ -11,6 +13,6 @@ const userRouter = express
   .get('/api/users', getAllUsers)
   .post('/api/users/register', registerUser)
   .post('/api/users/login', loginUser)
-  .post('/api/users/logout', logoutUser);
-// .get('/api/users/auth', isUserAuthenticated, getLoggedInUser);
+  .post('/api/users/logout', logoutUser)
+  .get('/api/users/auth', isUserAuthenticated, getLoggedInUser);
 export default userRouter;
