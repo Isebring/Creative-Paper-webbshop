7638144cc584e6346c5dfd2f010873307e006083
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-26 13:00:42 +0200

    changed name of file

--- client/src/contexts/UseShoppingCart.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { ShoppingCartContext } from './ShoppingCartContext';
-
-export function useShoppingCart() {
-  return useContext(ShoppingCartContext);
-}
