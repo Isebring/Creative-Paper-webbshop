63c7fb4d3921cebf5b6bff0c891b33f1e610c0a2
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-30 09:19:37 +0200

    added index to key

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -75,7 +75,11 @@ const ProductCard: React.FC<ProductCardProps> = ({ productId }) => {
                   product.summary
                     .split(',')
                     .map((item) => item.trim())
-                    .map((item) => <List.Item key={item}>{item}</List.Item>)}
+                    .map((item, index) => (
+                      <List.Item key={`${item.trim()}-${index}`}>
+                        {item.trim()}
+                      </List.Item>
+                    ))}
               </List>
             </Box>
           </Link>
