b6db875653f7c5756b59557cd094916b060cf0b8
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-31 20:05:34 +0200

    added loader

--- client/src/components/Protected.tsx
+++ client/src/components/Protected.tsx
@@ -1,3 +1,4 @@
+import { Loader } from '@mantine/core';
 import { ReactNode, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
@@ -11,7 +12,7 @@ function Protected({ children }: Props) {
   const { user, isLoading } = useContext(UserContext); // get user from UserContext
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loader color="violet" />;
   }
 
   if (user && user.isAdmin) {
