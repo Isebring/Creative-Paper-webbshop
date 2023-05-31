90b36859722b20a8491b709463e734757a873777
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-31 10:51:28 +0200

    added imageId to users order history

--- client/src/components/MyOrders.tsx
+++ client/src/components/MyOrders.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Table, Text } from '@mantine/core';
+import { Box, Divider, Image, Table, Text } from '@mantine/core';
 import { useEffect } from 'react';
 import { useOrderContext } from '../contexts/UseOrderContext';
 
@@ -40,7 +40,11 @@ function MyOrders() {
               <Text>Title: {item.product.title}</Text>
               <Text>Price per item: ${item.product.price}</Text>
               <Text>Quantity: {item.quantity}</Text>
-              {/* <Image src={item.product.image} width={100} fit="cover" /> */}
+              <Image
+                src={'/api/image/' + item.product.imageId}
+                width={100}
+                fit="cover"
+              />
               <Text>Total price: ${item.price}</Text>
               <Divider my="sm" variant="dotted" />
             </div>
