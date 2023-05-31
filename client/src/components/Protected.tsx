86eaa95962b7a2cceb772a9290302043da8194e9
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-05-31 18:46:25 +0200

    added is loading so admin can write /admin manually in the url

--- client/src/components/Protected.tsx
+++ client/src/components/Protected.tsx
@@ -2,13 +2,17 @@ import { ReactNode, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 
-interface ProtectedProps {
+interface Props {
   children: ReactNode;
 }
 
 // Function for protected route for admin
-function Protected({ children }: ProtectedProps) {
-  const { user } = useContext(UserContext); // get user from UserContext
+function Protected({ children }: Props) {
+  const { user, isLoading } = useContext(UserContext); // get user from UserContext
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (user && user.isAdmin) {
     return <>{children}</>;
