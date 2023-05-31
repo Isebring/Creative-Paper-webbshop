903bde04b24fa9b8dda2e820c7a5b413f0806363
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-31 13:38:14 +0200

    responsive adminordertable

--- client/src/components/AdminOrderTable.tsx
+++ client/src/components/AdminOrderTable.tsx
@@ -1,13 +1,16 @@
-import { Box, Divider, Select, Table, Text } from '@mantine/core';
+import { Box, Divider, List, Select, Table, Text, useMantineColorScheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { useEffect, useState } from 'react';
 import { useOrderContext } from '../contexts/UseOrderContext';
 
 function AdminOrders() {
   const { getAllOrders, orders, updateOrderStatus } = useOrderContext();
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
   const [localStatuses, setLocalStatuses] = useState<{
     [key: string]: 'in progress' | 'shipped';
   }>({});
 
+  
   const updateLocalStatus = (
     _id: string,
     status: 'in progress' | 'shipped',
@@ -23,7 +26,7 @@ function AdminOrders() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const rows =
+    const tableRows =
     Array.isArray(orders) &&
     orders.map((order) => (
       <tr key={order._id}>
@@ -77,21 +80,68 @@ function AdminOrders() {
       </tr>
     ));
 
+    const listRows = 
+    Array.isArray(orders) &&
+    orders.map((order) => (
+      <List.Item key={order._id}>
+        <Box style={{ width: '100%' }}>
+          <Text fw={700}>Order ID: {order._id}</Text>
+          <Text>Shipping Details: {order.deliveryAddress.email}</Text>
+          <Text>Total Items: {order.orderItems.reduce((sum, item) => sum + item.quantity, 0)}</Text>
+          <Text>Order Items:</Text>
+          {order.orderItems.map((item, index) => (
+            <Box key={`${item.product._id}-${index}`}>
+              <Text>
+                Product <Text fw={700}>{item.product._id}</Text>
+              </Text>
+              <Text>Title: {item.product.title}</Text>
+              <Text>Price per item: ${item.product.price}</Text>
+              <Text>Quantity: {item.quantity}</Text>
+              <Text>Total price: ${item.price}</Text>
+              <Divider my="sm" variant="dotted" />
+            </Box>
+          ))}
+          <Text>Order Total: ${order.totalPrice}</Text>
+          <Text>Order Date: {new Date(order.createdAt).toLocaleDateString()}</Text>
+          <Text>Status:</Text>
+          <Box style={{ width: '350px' }}>
+            <Select
+              value={localStatuses[order._id] || order.status}
+              onChange={(value) => {
+                updateLocalStatus(order._id, value as 'in progress' | 'shipped');
+                updateOrderStatus(order._id, value as 'in progress' | 'shipped');
+              }}
+              data={[
+                { value: 'in progress', label: 'In Progress' },
+                { value: 'shipped', label: 'Shipped' },
+              ]}
+            />
+          </Box>
+          <Divider size="lg" mt="lg" mb="lg" />
+        </Box>
+      </List.Item>
+    ));
+
+
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Order ID</th>
-          <th>Shipping Details</th>
+    isDesktop ? (
+      <Table>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Shipping Details</th>
           <th>Total Items</th>
           <th>Order Items</th>
           <th>Order Total</th>
           <th>Order Date</th>
           <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </Table>
+    ) : (
+      <List style={{ listStyle: 'none', }}>{listRows}</List>
+    )
   );
 }
 
